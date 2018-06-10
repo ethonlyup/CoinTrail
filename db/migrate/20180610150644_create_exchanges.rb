7ee7c383f338d4b638cf00f566858899900f7dd6
Kimchee <ChristopherPagnusatt@gmail.com>
2018-06-10 18:07:48 +0300

    Added Exchange model

---
+++ db/migrate/20180610150644_create_exchanges.rb
@@ -0,0 +1,10 @@
+class CreateExchanges < ActiveRecord::Migration[5.2]
+  def change
+    create_table :exchanges do |t|
+      t.string :base_endpoint
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
