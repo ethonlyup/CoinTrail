951b4f2a8802026f623637301d511282f548d409
Ben Abitbul <benabitbul@hotmail.com>
2018-06-10 17:59:29 +0300

    apis controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'apis/create'
+  get 'apis/destroy'
+  get 'apis/new'
+  get 'apis/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
