b256e8f5c9fa6c622ccbba79e19a9b6b2d6f06a9
Kimchee <ChristopherPagnusatt@gmail.com>
2018-06-10 18:46:40 +0300

    Added Alert model

---
+++ db/migrate/20180610154608_create_alerts.rb
@@ -0,0 +1,11 @@
+class CreateAlerts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :alerts do |t|
+      t.string :coin_name
+      t.float :price_above
+      t.float :price_below
+
+      t.timestamps
+    end
+  end
+end
