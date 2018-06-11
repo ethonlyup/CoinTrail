ee1139d8eecca4be0178528065a7b5db23c90c14
Ben Abitbul <benabitbul@hotmail.com>
2018-06-11 10:43:39 +0300

    created new for api controller

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,13 +1,14 @@
 class ApisController < ApplicationController
+  def new
+    @api = Api.new
+  end
+
+  def update
+  end
+
   def create
   end
 
   def destroy
-  end
-
-  def new
-  end
-
-  def update
   end
 end
