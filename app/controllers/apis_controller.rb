390232c6ed88009dd5961e42f7e337300241382a
Ben Abitbul <benabitbul@hotmail.com>
2018-06-11 14:29:58 +0300

    apis controller and form adjustment

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,13 +1,33 @@
 class ApisController < ApplicationController
-  def update
+  before_action :set_api, only: [:edit, :destroy]
+
+  def new
+    @api = Api.new
   end
 
   def create
+    @api = Api.new(api_params)
+    if @api.save
+      redirect_to @api, notice: 'API connection was successfully created'
+    else
+      render :new
+    end
+  end
+
+  def update
   end
 
   def destroy
   end
+
+  private
+
+  def set_api
+    @api = Api.find(params[:id])
+  end
+
+  def api_params
+    params.require(:api).permit(:publishable_key, :secret_key, :exchange)
+  end
 end
 
-def edit
-end
