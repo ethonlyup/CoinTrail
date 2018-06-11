7af69312c5f3cfa77368422e2786f1b4cdf2ec77
Ben Abitbul <benabitbul@hotmail.com>
2018-06-11 12:07:54 +0300

    deleted new method in api controller

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,12 +1,4 @@
 class ApisController < ApplicationController
-  def new
-    if current_user.publishable_key && current_user.secret_key
-      @api = Api.new
-    else
-      notice: 'Please ensure both fields are entered before creating an API connection'
-    end
-  end
-
   def update
   end
 
