51c5b8bbb4545ddc6322d7619be455495ed576e9
Ben Abitbul <benabitbul@hotmail.com>
2018-06-12 12:12:56 +0300

    created dashboard route, created dashboard method in pages controller, created dashboard HTML, created method that redirects to user dashboard if user is already logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if user_signed_in?
+      redirect_to dashboard_path
+    end
+  end
+
+  def dashboard
   end
 end
