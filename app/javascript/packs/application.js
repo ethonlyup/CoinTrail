93cc83398763dd7b2f548fc67d45769c775b3391
sarah bokobza <sarahbokobza@gmail.com>
2018-06-12 18:42:55 +0300

    hp done, dropdown login and register

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,4 +4,4 @@ import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
 
-import url('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css');
+
