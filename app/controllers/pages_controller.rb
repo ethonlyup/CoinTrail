34db0ded23a9c0ddd0fc828ba75967fbee299b32
Ben Abitbul <benabitbul@hotmail.com>
2018-06-19 12:45:51 +0300

    added api functionality for dynamic prices, added dynamic 24% changes, adjusted gemfile to correct gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,17 +7,32 @@ class PagesController < ApplicationController
     end
   end
 
-  def market_data
+  def market_api
+    @total_market = Coinmarketcap.coins(1000)
+  end
 
+  def market_price(b)
+    b[:asset] = 'MIOTA' if b[:asset] == 'IOTA'
+    @total_market.select {|a| a["symbol"] == "#{b[:asset]}"}.first["price_usd"].to_f
+  rescue
+    0
+  end
+
+  def market_change(b)
+    b[:asset] = 'MIOTA' if b[:asset] == 'IOTA'
+    @total_market.select {|c| c["symbol"] == "#{b[:asset]}"}.first["percent_change_24h"].to_f
+    rescue
+      0
   end
 
   def dashboard
     if current_user.apis.exists?
-    binance_balance
-    bittrex_balance
-    set_total
-  else
-    redirect_to new_api_path, notice: "Please add an api before using dashboard"
+      market_api
+      binance_balance
+      bittrex_balance
+      set_total
+    else
+      redirect_to new_api_path, notice: "Please add an api before using dashboard"
     end
   end
 
@@ -28,57 +43,57 @@ class PagesController < ApplicationController
     @binance_account = Binance::Api.info!
     @binance_balance = Array.new
     @binance_account[:balances].each do |b|
-      if (b[:free]).to_f > 0.00001
-        price = rand(10..6500)
-        @binance_balance<< {
-          asset: b[:asset],
-          price: price,
-          holding: (b[:locked].to_f + b[:free].to_f).round(5),
-          total: (b[:locked].to_f + b[:free].to_f) * price
-        }
-      end
+    if (b[:free]).to_f > 0.00001 && b[:asset] != "EON"
+      @binance_balance << {
+        asset: b[:asset],
+        price: market_price(b),
+        holding: (b[:locked].to_f + b[:free].to_f).round(5),
+        total: (b[:locked].to_f + b[:free].to_f) * market_price(b),
+        change: market_change(b)
+      }
     end
   end
+end
 
-  def bittrex_balance
-    set_keys_bittrex
-    Bittrex.config do |c|
-      c.key = @bittrex_publishable
-      c.secret = @bittrex_secret
-    end
-    @bittrex_balance = Array.new
-    @bittrex_account = Bittrex::Wallet.all
-    @bittrex_account.each do |c|
-      if c.available + c.pending > 0.00000000
-        price = rand(10..6500)
-        @bittrex_balance << {
-          asset: c.raw["Currency"],
-          price: price,
-          holding: c.available,
-          total: c.available * price
-        }
-      end
+def bittrex_balance
+  set_keys_bittrex
+  Bittrex.config do |c|
+    c.key = @bittrex_publishable
+    c.secret = @bittrex_secret
+  end
+  @bittrex_balance = Array.new
+  @bittrex_account = Bittrex::Wallet.all
+  @bittrex_account.each do |b|
+    if b.available + b.pending > 0.00000000
+      @bittrex_balance << {
+        asset: b.raw["Currency"],
+        price: market_price(b),
+        holding: b.available,
+        total: b.available * market_price(b)
+      }
     end
   end
+end
 
-  def set_total
-    @total_balance = @binance_balance.dup
-    @bittrex_balance.each do |c|
-      exists = @binance_balance.index {|i| i[:asset] == c[:asset]}
-      if exists.present?
-       total = @total_balance[exists][:holding] + c[:holding]
-       @total_balance[exists] = {
-         asset: c[:asset],
-         price: c[:price],
-         holding: total,
-         total: total * c[:price]
-       }
-     else
-      @total_balance << c
-    end
+def set_total
+  @total_balance = @binance_balance.dup
+  @bittrex_balance.each do |b|
+    exists = @binance_balance.index {|i| i[:asset] == b[:asset]}
+    if exists.present?
+     total = @total_balance[exists][:holding] + b[:holding]
+     @total_balance[exists] = {
+       asset: b[:asset],
+       price: market_price(b),
+       holding: total,
+       total: total * market_price(b),
+       change: market_change(b)
+     }
+   else
+    @total_balance << b
   end
-  @total_balance.sort_by! { |z| z[:total] }.reverse!
-  @total_sum = @total_balance.map {|h| h[:total]}.sum.round(2)
+end
+@total_balance.sort_by! { |z| z[:total] }.reverse!
+@total_sum = @total_balance.map {|h| h[:total]}.sum.round(2)
 end
 
 def set_keys_bittrex
