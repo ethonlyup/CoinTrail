a405eb5877a36af4eb2c2221f02a8beb4736f043
Ben Abitbul <benabitbul@hotmail.com>
2018-06-20 19:54:11 +0300

    added alert functionality, added notification on and off, fixed alerts view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    if user_signed_in?
-      redirect_to new_api_path
-    end
+    # if user_signed_in?
+    #   redirect_to new_api_path
+    # end
   end
 
   def market_api
@@ -51,7 +51,8 @@ class PagesController < ApplicationController
         price: market_price(b),
         holding: (b[:locked].to_f + b[:free].to_f).round(5),
         total: (b[:locked].to_f + b[:free].to_f) * market_price(b),
-        change: market_change(b)
+        change: market_change(b),
+        alert: Alert.where(user: current_user, coin_name: b[:asset]).present?
       }
     end
   end
