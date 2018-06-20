5e12b99f5b9d78e1a79a0cf871f0166472fb7ffa
Kimchee <ChristopherPagnusatt@gmail.com>
2018-06-20 09:10:21 +0300

    Fixed BCC market price

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,7 @@ class PagesController < ApplicationController
   end
 
   def market_price(b)
+    b[:asset] = "BCH" if b[:asset] == "BCC"
     b[:asset] = 'MIOTA' if b[:asset] == 'IOTA'
     @total_market.select {|a| a["symbol"] == "#{b[:asset]}"}.first["price_usd"].to_f
   rescue
@@ -19,6 +20,7 @@ class PagesController < ApplicationController
   end
 
   def market_change(b)
+    b[:asset] = "BCH" if b[:asset] == "BCC"
     b[:asset] = 'MIOTA' if b[:asset] == 'IOTA'
     @total_market.select {|c| c["symbol"] == "#{b[:asset]}"}.first["percent_change_24h"].to_f
     rescue
